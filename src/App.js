52b198e91989564f7cb85ae63abd8c2946babd38
saurabhpandey2003 <saurabhslam755@gmail.com>
2023-09-25 15:46:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import SpeechRecognition from 'react-speech-recognition';
+import { useSpeechRecognition } from 'react-speech-recognition';
+import useClipboard from 'react-use-clipboard';
+import { useState } from 'react';
+
+function App() {
+
+  const [textcopied, settextcopied] = useState();
+  const [isCopied, setCopied] = useClipboard(textcopied);
+  const startlisting = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
+  const stoplisting = () => SpeechRecognition.stopListening()
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
+  const funcsettextcopied = () => {
+    settextcopied(transcript)
+  };
+  return (
+    <>
+      <div className='container'>
+        <div className='heading_area'>
+          <h1>Speech to Text Converter</h1>
+        </div>
+
+        <div className='text_area' onClick={funcsettextcopied}>{transcript}</div>
+        <div className='btn'>
+          <button className='button' onClick={setCopied}>was copied ?{isCopied ? "yes" : "nope"}</button>
+          <button className='button' onClick={startlisting}>start listening</button>
+          <button className='button' onClick={stoplisting}>stop listing</button>
+        </div>
+
+      </div>
+
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import SpeechRecognition from 'react-speech-recognition';
+ReactDOM.render(
+  <>
+  <App></App>
+
+  </>,document.getElementById("root")
+);
+
